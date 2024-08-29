49c6f658f4d57a0a8a8d0ec933214b5728ec6fbc
BrianT <304@DESKTOP-OBFHSVF>
2024-08-29 14:43:38 -0700

    first commit

---
+++ Test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I have a number between 1 and 1000.\n";
+    std::cout << "Can you guess my number?\n";
+    std::cout << "Please type your first guess.\n";
+    std::cout << "?";
+
+    for (int i = 1; i < 1000; i++){
+
+        int x;
+        std::cout << "";
+        std::cin >> x;
+
+        if (x == 23) {
+            std::cout << "Excellent! You guessed the number!\n";
+            break;
+        } else if (x < 23) {
+            std::cout << "Too low. Try again.\n";
+            std::cout << "?";
+        } else if (x > 23) {
+            std::cout << "Too high. Try again.\n";
+            std::cout << "?";
+        } else {
+            std::cout << "Invalid Input";
+        }
+
+    }
+    
+    return 0;
+
+}
